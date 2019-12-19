7b2ce62459c8d5da5e342003a8ce206e85dfc7eb
Your Name <you@example.com>
2019-12-19 16:33:58 +0200

    new commit

---
+++ app/src/main/java/com/besolutions/konsil/personal_info/personal_info.java
@@ -0,0 +1,15 @@
+package com.besolutions.konsil.personal_info;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.besolutions.konsil.R;
+
+public class personal_info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.personal_info);
+    }
+}
