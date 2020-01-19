e8b2e74f5366cf57a507032fe3e41fcecd5d1c84
Your Name <you@example.com>
2020-01-19 14:51:15 +0200

    new commit

--- app/src/main/java/com/besolutions/konsil/scenarios/scenario_splash_screen/Controller/splash_screen.java
+++ app/src/main/java/com/besolutions/konsil/scenarios/scenario_splash_screen/Controller/splash_screen.java
@@ -9,7 +9,9 @@ import android.view.View;
 import android.widget.Button;
 
 import com.besolutions.konsil.R;
+import com.besolutions.konsil.local_data.saved_data;
 import com.besolutions.konsil.scenarios.scenario_login.Controller.login;
+import com.besolutions.konsil.scenarios.scenario_mian_page.Controller.main_screen;
 import com.besolutions.konsil.scenarios.scenario_splash_screen.pattern.viewimage;
 import com.besolutions.konsil.scenarios.scenario_welcome_tour.scenario_welcome_tour;
 import com.besolutions.konsil.utils.utils;
@@ -22,6 +24,7 @@ public class splash_screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.scenario_welcome_tour);
+        new utils().set_language(new saved_data().get_lan(splash_screen.this),splash_screen.this);
         final ViewPager  viewPager = (ViewPager) findViewById(R.id.viewpager);
         viewimage = new viewimage(splash_screen.this);
         viewPager.setAdapter(viewimage);
