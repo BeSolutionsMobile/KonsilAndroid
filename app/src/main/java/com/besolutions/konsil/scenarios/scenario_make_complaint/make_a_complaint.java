cf7084707e9c86f938bf52facd16d2a734784953
Your Name <you@example.com>
2020-01-28 16:25:02 +0200

    new commit

--- app/src/main/java/com/besolutions/konsil/scenarios/scenario_make_complaint/make_a_complaint.java
+++ app/src/main/java/com/besolutions/konsil/scenarios/scenario_make_complaint/make_a_complaint.java
@@ -4,19 +4,27 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.besolutions.konsil.R;
 import com.besolutions.konsil.scenarios.scenario_mian_page.Controller.main_screen;
 
+import java.util.ArrayList;
+
 public class make_a_complaint extends AppCompatActivity implements View.OnClickListener {
     Button submit;
+    Spinner make_complaint_spi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.make_a_complaint);
         submit=(Button)findViewById(R.id.make_complaint);
+        make_complaint_spi=(Spinner)findViewById(R.id.make_complaint_spi);
+
+        set_spinner_data();
         submit.setOnClickListener(this);
 
         TextView   title=(TextView)findViewById(R.id.title);
@@ -32,5 +40,17 @@ public class make_a_complaint extends AppCompatActivity implements View.OnClickL
         {
             startActivity(new Intent(this, main_screen.class));
         }
+    }
+
+    //ADD DATA TO SPINNER
+    void set_spinner_data()
+    {
+        ArrayList<String> arrayList=new ArrayList<>();
+        arrayList.add("first reason");
+        arrayList.add("second reason");
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_dropdown_item,arrayList);
+        make_complaint_spi.setAdapter(adapter);
+
     }
 }
--- app/src/main/java/com/besolutions/konsil/scenarios/scenario_payment/controller/payment.java
+++ app/src/main/java/com/besolutions/konsil/scenarios/scenario_payment/controller/payment.java
@@ -3,23 +3,33 @@ package com.besolutions.konsil.scenarios.scenario_payment.controller;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.besolutions.konsil.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class payment extends AppCompatActivity implements View.OnClickListener {
    Button pay;
+   Spinner cardtype;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.payment);
         pay=(Button)findViewById(R.id.pay);
+        cardtype=(Spinner)findViewById(R.id.cardtype);
         pay.setOnClickListener(this);
 
         TextView title =(TextView)findViewById(R.id.title);
         String Payment = getResources().getString(R.string.payment);
         title.setText(Payment);
+
+        //SET SPINNER DATA
+        set_spinner_data();
     }
 
     @Override
@@ -29,5 +39,17 @@ public class payment extends AppCompatActivity implements View.OnClickListener {
            loading loading=new loading();
            loading.dialog(payment.this,R.layout.successful_payment,.80);
         }
+    }
+
+    //ADD DATA TO SPINNER
+    void set_spinner_data()
+    {
+        ArrayList<String>arrayList=new ArrayList<>();
+        arrayList.add("VISA");
+        arrayList.add("MASTER CARD");
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_dropdown_item,arrayList);
+        cardtype.setAdapter(adapter);
+
     }
 }
