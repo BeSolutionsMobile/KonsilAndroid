ba73530a16a7523665dd768db6d7cb9cb514d8cd
mac <amirahmedmorsy@yahoo.com>
2020-02-06 15:04:33 +0200

    Amir Edits Commit

--- app/src/main/java/com/besolutions/konsil/scenarios/scenario_doctor_list/Controller/doctor_list.java
+++ app/src/main/java/com/besolutions/konsil/scenarios/scenario_doctor_list/Controller/doctor_list.java
@@ -55,11 +55,11 @@ public class doctor_list extends AppCompatActivity implements View.OnClickListen
 
         //ADD TO ARRAY LIST
         ArrayList<doctor_list_items> arrayList=new ArrayList<>();
-        arrayList.add(new doctor_list_items("1","ahmed awad","prof","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfxzJ84GqW4XD_88NeeOonmUWr1seByDHl0WJkYqk05DVlInfZ&s",5));
-        arrayList.add(new doctor_list_items("1","mahmoud ibraheem","prof","https://www.thehealthy.com/wp-content/uploads/2017/09/02_doctor_Insider-Tips-to-Choosing-the-Best-Primary-Care-Doctor_519507367_Stokkete.jpg",3));
-        arrayList.add(new doctor_list_items("1","saeed awad","prof","https://img.medscape.com/thumbnail_library/dt_140605_serious_male_doctor_hospital_800x600.jpg",3));
-        arrayList.add(new doctor_list_items("1","mahmoud el ahmady","prof","https://metrosource.com/wp-content/uploads/2018/05/lgbt-friendly-doctor.jpg",3));
-        arrayList.add(new doctor_list_items("1","fathi yaseen awad","prof","https://img.medscape.com/thumbnail_library/dt_181213_sad_depressed_pensive_doctor_800x450.jpg",3));
+        arrayList.add(new doctor_list_items("1","ahmed awad",getString(R.string.doctor),"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfxzJ84GqW4XD_88NeeOonmUWr1seByDHl0WJkYqk05DVlInfZ&s",5));
+        arrayList.add(new doctor_list_items("1","mahmoud ibraheem",getString(R.string.doctor),"https://www.thehealthy.com/wp-content/uploads/2017/09/02_doctor_Insider-Tips-to-Choosing-the-Best-Primary-Care-Doctor_519507367_Stokkete.jpg",3));
+        arrayList.add(new doctor_list_items("1","saeed awad",getString(R.string.doctor),"https://img.medscape.com/thumbnail_library/dt_140605_serious_male_doctor_hospital_800x600.jpg",3));
+        arrayList.add(new doctor_list_items("1","mahmoud el ahmady",getString(R.string.doctor),"https://metrosource.com/wp-content/uploads/2018/05/lgbt-friendly-doctor.jpg",3));
+        arrayList.add(new doctor_list_items("1","fathi yaseen awad",getString(R.string.doctor),"https://img.medscape.com/thumbnail_library/dt_181213_sad_depressed_pensive_doctor_800x450.jpg",3));
 
         doctor_list=(RecyclerView)findViewById(R.id.doctor_list);
 
